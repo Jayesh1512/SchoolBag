ee88cda45a7a9c2186d1ffa6fa055248f75bd695
Jayesh1512 <jayeshshetestudy@gmail.com>
2024-07-21 04:19:06 +0530

    signup init

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 const page = () => {
   return (
-    <div className='grid place-content-center h-screen'>
+    <div className='grid place-content-center h-screen text-white'>
       <Link href="/login">
         <button>Login</button>
       </Link>
