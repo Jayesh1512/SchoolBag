981cd278cf678dd206a5ce26475a035f8674cde6
IshadP <ishadpande16@gmail.com>
2024-07-21 23:05:28 +0530

    changed tailwind.config.js. Added color variables!

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "yellow" : '#FCFF5A',
-        "background" : "#191919"
-      },
+        "brand-primary-100" : '#FDFF8F',
+        "brand-primary-200" : '#FCFF5A',
+        "brand-primary-300" : '#E9EC52',
+        "brand-primary-400" : '#CCCE48',
+        "brand-primary-500" : '#A3A536',
+        "base-100" : "#595959",
+        "base-200" : "#494949",
+        "base-300" : "#393939",
+        "base-400" : "#292929",
+        "base-500" : "#191919",
+        "white-100" : "#FFFFFF",
+        "white-200" : "#F0F0F0",
+        "white-300" : "#DFDFDF",
+        "white-400" : "#BEBEBE",
+        "white-500" : "#9A9A9A",
+        "Alert" : "#FF4949",
+        "Success" : "#00CC03",
+        "Warning" : "#FAAD1D",
+        "Information" : "1D90FA"
+
+       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
