fbf068b01c8204c916bc78fead0b2d2e169a7063
IshadP <ishadpande16@gmail.com>
2024-07-22 00:19:45 +0530

    Complete all css custom classes for fonts. Made folders for components according to figma files. Changes some code for login screen for testing. Everything works apprantly!

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,24 +4,26 @@ import { LuMail } from "react-icons/lu";
 
 const page = () => {
   return (
-    <div className="bg-background max-h-screen text-center lg:w-1/5 mx-auto max-sm:w-full w-1/2 relative max-sm:-top-10 overflow-hidden">
-      <img src="/img/login/main/bag.png" alt="" className="relative" />
-      <p className="text-yellow font-medium text-xl py-5">
-        All Your Learning, One Place
-      </p>
+    <div className="bg-base-500 h-screen flex flex-col text-center lg:w-1/5 mx-auto max-sm:w-full w-1/2 relative max-sm:-top-10 overflow-hidden gap-20">
+      <div className="flex flex-col justify-center items-center gap-6">
+        <img src="/img/login/main/bag.png" alt="" className="relative" />
+        <p className="title-1 text-brand-primary-200">
+          All Your Learning, One Place
+        </p>
+      </div>
       <div className=" flex justify-center ">
         <div className="flex flex-col items-center w-11/12">
           <button className="text-white flex justify-center w-full items-center gap-4 py-2 border rounded-lg font-medium text-base">
             <FcGoogle />
-            <span className="">Login Using Google</span>
+            <span className="title-3">Login Using Google</span>
           </button>
           <p className="text-[#595959] font-medium py-4 ">OR</p>
           <button className="text-white flex justify-center w-full items-center gap-4 py-2 border rounded-lg font-medium text-base">
-            <p className="">Login Using email</p>
+            <p className="title-3">Login Using email</p>
           </button>
-          <button className="text-black bg-yellow flex justify-center mt-6 w-full items-center gap-4 py-2 border border-black rounded-lg font-medium text-base">
+          <button className="text-black  bg-brand-primary-200 flex justify-center mt-6 w-full items-center gap-4 py-2 border border-none rounded-lg font-medium text-base">
             <LuMail />
-            <Link href='/signup' className="overflow-hidden">Signup Using email</Link>
+            <Link href='/signup' className="title-3">Signup Using email</Link>
           </button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,33 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'SpaceGrotesk' : ['Space+Grotesk', 'sans-serif'] 
+      },
+      fontSize: {
+        'lt': '2.125rem',
+        't1' : '1.75rem',
+        't2' : '1.375rem',
+        't3' : '1.25rem',
+        'body' : '1.0625rem',
+        'cout' : '1rem',
+        'subh' : '0.9375rem',
+        'fnot' : '0.8125rem',
+        'cap1' : '0.75rem',
+        'cap2' : '0.6875rem',
+      },
+      lineHeight: {
+        'lt': '2.5625rem',
+        't1' : '2.125rem',
+        't2' : '1.75rem',
+        't3' : '1.5625rem',
+        'body' : '1.375rem',
+        'cout' : '1.3125rem',
+        'subh' : '1.25rem',
+        'fnot' : '1.125rem',
+        'cap1' : '1rem',
+        'cap2' : '0.8125rem',
+      }
     },
   },
   plugins: [],
